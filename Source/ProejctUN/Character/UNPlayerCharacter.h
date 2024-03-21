9057b59d5b4e3292ec7bc871cd363c5968c4f851
leejimin3 <ttette1559@naver.com>
2024-03-22 03:31:16 +0900

    [ADD] PlayerCharacter Class

---
+++ Source/ProejctUN/Character/UNPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Character/UNCharacter.h"
+#include "UNPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROEJCTUN_API AUNPlayerCharacter : public AUNCharacter
+{
+	GENERATED_BODY()
+	
+};
