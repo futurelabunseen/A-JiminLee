8ac601e7f2a7f63f2187f5df72c3b26767076153
leejimin3 <ttette1559@naver.com>
2024-03-27 02:14:20 +0900

    [FIX] FIX Build.cs include

--- Source/ProejctUN/ProejctUN.Build.cs
+++ Source/ProejctUN/ProejctUN.Build.cs
@@ -8,11 +8,11 @@ public class ProejctUN : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicIncludePaths.AddRange(new string[] { "ProjectUN" });
+        PublicIncludePaths.AddRange(new string[] { "ProejctUN" });
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags", "GameFeatures" });
+        PublicDependencyModuleNames.AddRange(new string[] {"ProejctUN", "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput", "GameplayAbilities", "GameplayTasks", "GameplayTags", "GameFeatures" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "ProejctUN" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
