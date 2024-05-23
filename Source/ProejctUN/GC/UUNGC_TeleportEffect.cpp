334c8470b56282511ca1a773683a4b3532f5b988
leejimin3 <ttette1559@naver.com>
2024-05-23 09:23:17 +0900

    [FIX] 캐릭터가 새로운 맵에서 땅에 박히는 현상 해결

--- Source/ProejctUN/GC/UUNGC_TeleportEffect.cpp
+++ Source/ProejctUN/GC/UUNGC_TeleportEffect.cpp
@@ -16,18 +16,15 @@ UUUNGC_TeleportEffect::UUUNGC_TeleportEffect()
 
 bool UUUNGC_TeleportEffect::OnExecute_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) const
 {
-	GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "1");
 	const AActor* SourceASC = Parameters.EffectContext.GetInstigator();
 	if (SourceASC)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "2");
 		UGameplayStatics::SpawnEmitterAtLocation(Target, ParticleSystem, SourceASC->GetActorLocation(), FRotator::ZeroRotator, true);
 	}
 
 	const FHitResult* HitResult = Parameters.EffectContext.GetHitResult();
 	if (HitResult)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "3");
 		UGameplayStatics::SpawnEmitterAtLocation(Target, ParticleSystem, HitResult->Location, FRotator::ZeroRotator, true);
 	}
 
